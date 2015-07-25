d061d1bb5fcee8397cbad147cfc1ed9a2a003d96
sub77 <sub77@ymail.com>
2015-07-25 17:16:54 +0200

    new

--- bliss.mk
+++ bliss.mk
@@ -1,7 +1,10 @@
-$(call inherit-product, device/samsung/matissewifi/full_matissewifi.mk)
+#Bliss Audio Mod
+BLISS_AUDIO_MOD := viper
 
-# Inherit some common slim stuff.
 $(call inherit-product, vendor/bliss/config/common_full_tablet_wifionly.mk)
 
+$(call inherit-product, device/samsung/matissewifi/full_matissewifi.mk)
+
+PRODUCT_RELEASE_NAME := SM-T530
 PRODUCT_NAME := bliss_matissewifi
-PRODUCT_DEVICE := matissewifi
+
